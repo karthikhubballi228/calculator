472b38db4b7fa792b7d15686afcbaf832fe30989
karthik <01fe22bca228@kletech.ac.in>
2024-02-08 11:37:38 +0530

    code related to basic airth ops add two more

--- calculator.java
+++ calculator.java
@@ -15,6 +15,12 @@ public class calculator {
     float division(float operand_1, float operand_2){
         return(operand_1 / operand_2);
     }
+    float square(float operand_1){
+        return(operand_1 * operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
     public static void main(String[] args){
         float operand_1=10;
         float operand_2=5;
@@ -27,5 +33,9 @@ public class calculator {
         System.out.println("result of multiplication is:" +mul_result);
         float div_result=cal.division(operand_1, operand_2);
         System.out.println("result of division is:" +div_result);
+        float sq_result=cal.square(operand_1);
+        System.out.println("result of square is:"+sq_result);
+        float cub_result=cal.cube(operand_1);
+        System.out.println("result of cube is:" +cub_result);
     }
 }
